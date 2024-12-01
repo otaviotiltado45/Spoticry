1bf8db38e225ba4e0f1b14801815b4b8d94d2a74
otaviotiltado45 <otaviobarros.ad14@gmail.com>
2024-12-01 04:00:17 -0300

    endpoint login

--- spoticry/src/pages/Login.js
+++ spoticry/src/pages/Login.js
@@ -3,10 +3,34 @@ import React, { useState } from 'react';
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('Login attempted with', email, password);
+
+    const userData = { email, password };
+
+    try {
+      const response = await fetch('https://mqjnto3qw2.execute-api.us-east-1.amazonaws.com/default/user/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+      });
+
+      if (!response.ok) {
+        throw new Error('Login falhou');
+      }
+
+      const data = await response.json();
+      // Armazenar o token JWT no localStorage ou no estado global
+      localStorage.setItem('authToken', data.token);
+      alert('Login bem-sucedido!');
+      // Redirecionar ou fazer outras ações após o login bem-sucedido
+    } catch (error) {
+      setErrorMessage('Erro ao fazer login, tente novamente.');
+    }
   };
 
   return (
@@ -24,6 +48,7 @@ const Login = () => {
         onChange={(e) => setPassword(e.target.value)}
       />
       <button type="submit">Login</button>
+      {errorMessage && <p>{errorMessage}</p>}
     </form>
   );
 };
